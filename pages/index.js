630f34cb7df56ace7a1a713a37de4cd827db0479
3weeda <3weeda@gmail.com>
2020-12-13 22:06:31 +0200

    SYFT-640: separate layout component

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,16 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import styles from '../styles/Home.module.scss';
+import Layout from 'layout/Layout';
+import Preloader from 'components/Preloader';
 
-export default function Home() {
-  const [loading, setLoading] = useState(100);
-
-  useEffect(() => {
-    if (!loading) return;
-
-    const intervalId = setInterval(() => {
-      setLoading(loading - 1);
-    }, 100);
-
-    return () => clearInterval(intervalId);
-  }, [loading]);
-
+const index = () => {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>Scrappy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <div className={styles.screen}>
-          <div className={styles.card}>
-            <div className={styles.cont} style={{ height: `${loading}%` }}>
-              <img src="/assets/images/logo.png" alt="" />
-            </div>
-            <div className={styles.cont}>
-              <img src="/assets/images/logo2.png" alt="" />
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
+      <Preloader />
+    </Layout>
   );
-}
+};
+export default index;
