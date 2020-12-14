705bca6156f52d972a41e13237d6e1d701add0d3
3weeda <3weeda@gmail.com>
2020-12-14 21:05:54 +0200

    SYFT-640: fake preloader done

--- components/Preloader/index.js
+++ components/Preloader/index.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
+import styles from './index.module.scss';
 
 const Preloader = () => {
   const [loading, setLoading] = useState(100);
 
+  // TODO: Change to actual loading time for all assets
   useEffect(() => {
     if (!loading) return;
 
@@ -17,10 +19,10 @@ const Preloader = () => {
     <div className={styles.preloader}>
       <div className={styles.preloader__img}>
         <div className={styles.logo} style={{ height: `${loading}%` }}>
-          <img src="/assets/images/logo.png" alt="" />
+          <img src="/assets/images/logo-white.png" alt="" />
         </div>
         <div className={styles.logo}>
-          <img src="/assets/images/logo2.png" alt="" />
+          <img src="/assets/images/logo-red.png" alt="" />
         </div>
       </div>
       <div className={styles.preloader__text}>
