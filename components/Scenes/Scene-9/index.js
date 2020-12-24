0e29d265a95e6dd6bd158256830c57702e5d1127
3weeda <3weeda@gmail.com>
2020-12-24 22:38:52 +0200

    SYFT-734: scene 9

--- components/Scenes/Scene-9/index.js
+++ components/Scenes/Scene-9/index.js
@@ -1,7 +1,28 @@
+import styles from './index.module.scss';
+
 const Scene9 = () => {
   return (
-    <div>
-      <p>Scene 9</p>
+    <div className={styles.section}>
+      {/* <div className={styles.text}>
+        <p>
+          Scrappy was poor, and in school his classmates would tease and harass
+          him about it. He felt bad that his kicks were torn up and generic.
+        </p>
+      </div> */}
+      <div className={styles.text}>
+        <p>
+          All he wanted to rock were the same shoes as his skateboard heroes,
+          and the popular kids in his class.
+        </p>
+      </div>
+      <div className={styles.backdrop1} />
+      <div className={styles.peopleRight}>
+        <img src="/assets/images/scene9/3.png" />
+      </div>
+      <div className={styles.peopleLeft}>
+        <img src="/assets/images/scene9/2.png" />
+      </div>
+      <div className={styles.backdrop2} />
     </div>
   );
 };
