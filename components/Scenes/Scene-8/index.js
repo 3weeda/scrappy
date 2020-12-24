77c57d384cb728d745092ad8f73a05138df99d70
3weeda <3weeda@gmail.com>
2020-12-24 22:38:38 +0200

    SYFT-733: scene 8

--- components/Scenes/Scene-8/index.js
+++ components/Scenes/Scene-8/index.js
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import cx from 'classnames';
+import styles from './index.module.scss';
+
 const Scene8 = () => {
+  const [step, setStep] = useState(1);
+
+  const toggle = () => {
+    if (step !== 4) {
+      setStep(step + 1);
+    } else {
+      setStep(1);
+    }
+  };
+
   return (
-    <div>
-      <p>Scene 8</p>
+    <div
+      className={cx(styles.section, {
+        [styles.sectionStepTwo]: step === 2,
+        [styles.sectionStepThree]: step === 3,
+        [styles.sectionStepFour]: step === 3,
+      })}
+      onClick={toggle}
+    >
+      <div className={styles.backdrop1} />
+      <div className={styles.people}>
+        <img src="/assets/images/scene8/2.png" />
+      </div>
+      <div className={styles.character}>
+        <img src="/assets/images/scene8/3.png" />
+      </div>
     </div>
   );
 };
