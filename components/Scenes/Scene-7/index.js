47d8886ec545cc9998791b2923d0842f28a76aa2
3weeda <3weeda@gmail.com>
2020-12-24 22:38:02 +0200

    SYFT-732: scene 7

--- components/Scenes/Scene-7/index.js
+++ components/Scenes/Scene-7/index.js
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+import cx from 'classnames';
+import styles from './index.module.scss';
+
 const Scene7 = () => {
+  const [step, setStep] = useState(1);
+
+  const toggle = () => {
+    if (step === 1) {
+      setStep(2);
+    } else {
+      setStep(1);
+    }
+  };
   return (
-    <div>
-      <p>Scene 7</p>
+    <div
+      className={cx(styles.section, { [styles.sectionStepTwo]: step === 2 })}
+      onClick={toggle}
+    >
+      <div className={styles.backdrop1} />
+      <div className={styles.characterLeft}>
+        <img src="/assets/images/scene7/2.png" />
+      </div>
+      <div className={styles.characterRight}>
+        <img src="/assets/images/scene7/3.png" />
+      </div>
     </div>
   );
 };
