1d751099b2e1253680e2b752ba3e2f6e50694d2b
3weeda <3weeda@gmail.com>
2020-12-29 19:35:05 +0200

    SYFT-638: implement motion div component

--- components/MotionDiv/index.js
+++ components/MotionDiv/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.module.scss';
 
-const MotionDiv = ({ children }) => {
-  return <div className={styles.cover}>{children}</div>;
+const MotionDiv = ({ children, transitionData, noEntering, noLeaving }) => {
+  const {
+    percent,
+    transitionPercent,
+    isVisible,
+    isLeaving,
+    isEntering,
+    leavingPercent,
+    enteringPercent,
+  } = transitionData;
+
+  return (
+    <div
+      className={styles.cover}
+      style={{
+        opacity: isLeaving
+          ? leavingPercent && !noLeaving
+          : isEntering && !noEntering
+          ? enteringPercent
+          : 1,
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 MotionDiv.propTypes = {
   children: PropTypes.node.isRequired,
+  transitionData: PropTypes.object.isRequired,
+  noEntering: PropTypes.bool,
+  noLeaving: PropTypes.bool,
+};
+
+MotionDiv.defaultValues = {
+  noEntering: false,
+  noLeaving: false,
 };
 
 export default MotionDiv;
