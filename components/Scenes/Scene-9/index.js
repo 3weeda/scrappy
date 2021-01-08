c4255e61d74a4003b0ab291f1fc1c43e8039f041
3weeda <3weeda@gmail.com>
2021-01-09 01:04:16 +0200

    SYFT-903-SYFT-941: add space to text

--- components/Scenes/Scene-9/index.js
+++ components/Scenes/Scene-9/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import MotionDiv from 'components/MotionDiv';
+import MotionDiv from 'components/shared/MotionDiv';
 import styles from './index.module.scss';
 
 const Scene9 = ({ transitionData }) => {
@@ -29,9 +29,8 @@ const Scene9 = ({ transitionData }) => {
       >
         <div className={styles.text}>
           <p>
-            At the end of each school day, Scrappy would walk home from
-            class.Carrying with him, all that pent up anxiety about being
-            teased.
+            At the end of each school day, Scrappy would walk home from class.
+            Carrying with him, all that pent up anxiety about being teased.
           </p>
         </div>
         <div className={styles.backdrop} />
