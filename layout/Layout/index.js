00cbf73a887997f4818c0255cc312b3ffae89622
3weeda <3weeda@gmail.com>
2021-01-27 18:51:11 +0200

    SYFT-953: add steps footer

--- layout/Layout/index.js
+++ layout/Layout/index.js
@@ -1,16 +1,19 @@
+import Footer from 'layout/Footer';
 import PropTypes from 'prop-types';
 import Header from '../Header';
 import styles from './index.module.scss';
 
-const Layout = ({ children, hideLogo }) => (
+const Layout = ({ children, pageId, hideLogo }) => (
   <main className={styles.main}>
     <Header hideLogo={hideLogo} />
     {children}
+    <Footer pageId={pageId} />
   </main>
 );
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  pageId: PropTypes.string.isRequired,
   hideLogo: PropTypes.bool,
 };
 
