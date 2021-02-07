a25c5d4d08c365d6d504a2c3fb6843c9758399bc
3weeda <3weeda@gmail.com>
2021-02-07 22:57:25 +0200

    SYFT-1273: scene 8 scroll-driven animation

--- components/Scenes/Scene-8/index.js
+++ components/Scenes/Scene-8/index.js
@@ -1,24 +1,54 @@
+/* eslint-disable no-nested-ternary */
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import MotionDiv from 'components/shared/MotionDiv';
-import UseStepper from 'components/shared/useStepper';
+import UseTransitionStepper from 'components/shared/useTransitionStepper';
 import Text from 'components/shared/Text';
 import styles from './index.module.scss';
 
 const Scene8 = ({ transitionData }) => {
-  const step = UseStepper(transitionData, [3, 6, 8, 10]);
+  const {
+    currentStep,
+    stepTwo,
+    stepThree,
+    stepFour,
+  } = UseTransitionStepper(transitionData, [10, 30, 60, 80, 100]);
 
   return (
     <MotionDiv transitionData={transitionData}>
       <div
         className={cx(styles.section, {
-          [styles.stepTwo]: step === 2,
-          [styles.stepThree]: step === 3,
-          [styles.stepFour]: step === 4,
+          [styles.stepTwo]: currentStep !== 1,
         })}
       >
-        <div className={styles.backdrop} />
-        <Text maxW="550" size="35" top="15%" left="30%" visible={step === 2}>
+        <div
+          className={styles.backdrop}
+          style={{
+            backgroundSize:
+              currentStep === 2
+                ? `auto ${100 + 0.4 * stepTwo}%`
+                : currentStep === 3
+                ? `auto 140%`
+                : currentStep === 4
+                ? `auto ${140 - 0.4 * stepFour}%`
+                : 'auto 100%',
+            backgroundPosition:
+              currentStep === 3
+                ? `${100 - stepThree}% 100%`
+                : currentStep === 4
+                ? `${0.5 * stepFour}% 100%`
+                : currentStep === 5
+                ? '50% 100%'
+                : '100% 100%',
+          }}
+        />
+        <Text
+          maxW="550"
+          size="35"
+          top="15%"
+          left="30%"
+          visible={currentStep === 2}
+        >
           Around the period when he was stenciling, the city was considering
           defunding the arts program in schools. Scrappy felt this was an
           injustice and unfair to the artists in the community.
