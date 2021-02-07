ced023346637606906ecf10129debf56247fe1f5
3weeda <3weeda@gmail.com>
2021-02-07 18:33:27 +0200

    SYFT-960: scene 3 scroll driven animations

--- components/Scenes/Scene-3/index.js
+++ components/Scenes/Scene-3/index.js
@@ -1,27 +1,80 @@
+/* eslint-disable no-nested-ternary */
 import PropTypes from 'prop-types';
-import cx from 'classnames';
 import MotionDiv from 'components/shared/MotionDiv';
+import UseTransitionStepper from 'components/shared/useTransitionStepper';
 import styles from './index.module.scss';
 
-const Scene3 = ({ transitionData }) => (
-  <MotionDiv transitionData={transitionData}>
-    <div
-      className={cx(styles.section, {
-        [styles.stepTwo]: transitionData.isLeaving,
-      })}
-    >
-      <div className={styles.backdrop1} />
-      <div className={styles.backdrop2}>
-        <img src="/assets/images/scene3/2-1.png" alt="" />
-      </div>
-      <div className={styles.backgroundContainer}>
-        <div className={styles.backdrop3}>
-          <div className={styles.glare} />
+const Scene3 = ({ transitionData }) => {
+  const { currentStep, stepTwo } = UseTransitionStepper(transitionData, [
+    1,
+    40,
+    100,
+  ]);
+
+  return (
+    <MotionDiv transitionData={transitionData}>
+      <div className={styles.section}>
+        <div
+          className={styles.backdrop1}
+          style={{
+            opacity:
+              currentStep === 1
+                ? '0'
+                : currentStep === 2
+                ? `${stepTwo / 100}`
+                : '1',
+            transform:
+              currentStep === 1
+                ? 'translateY(100vh)'
+                : currentStep === 2
+                ? `translateY(${100 - stepTwo}vh)`
+                : 'translateY(0)',
+          }}
+        />
+        <div
+          className={styles.backdrop2}
+          style={{
+            opacity:
+              currentStep === 1
+                ? '0'
+                : currentStep === 2
+                ? `${stepTwo / 100}`
+                : '1',
+            transform:
+              currentStep === 1
+                ? 'translateY(-100vh)'
+                : currentStep === 2
+                ? `translateY(${stepTwo - 100}vh)`
+                : 'translateY(0)',
+          }}
+        >
+          <img src="/assets/images/scene3/2-1.png" alt="" />
+        </div>
+        <div
+          className={styles.backdrop3}
+          style={{
+            opacity:
+              currentStep === 1
+                ? '0'
+                : currentStep === 2
+                ? `${stepTwo / 100}`
+                : '1',
+            transform:
+              currentStep === 1
+                ? 'translateY(40vh)'
+                : currentStep === 2
+                ? `translateY(${40 - 0.4 * stepTwo}vh)`
+                : 'translateY(0)',
+          }}
+        >
+          <div className={styles.backgroundContainer}>
+            <div className={styles.glare} />
+          </div>
         </div>
       </div>
-    </div>
-  </MotionDiv>
-);
+    </MotionDiv>
+  );
+};
 
 Scene3.propTypes = {
   transitionData: PropTypes.object.isRequired,
