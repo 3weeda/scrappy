a007939b33cd420c48aefa9450ec744b55b89f71
3weeda <3weeda@gmail.com>
2021-02-07 22:57:00 +0200

    SYFT-1273: scene 5 scroll-driven animation

--- components/Scenes/Scene-5/index.js
+++ components/Scenes/Scene-5/index.js
@@ -1,20 +1,27 @@
+/* eslint-disable no-nested-ternary */
 import PropTypes from 'prop-types';
 import Image from 'next/image';
 import cx from 'classnames';
 import MotionDiv from 'components/shared/MotionDiv';
-import UseStepper from 'components/shared/useStepper';
+import UseTransitionStepper from 'components/shared/useTransitionStepper';
 import Text from 'components/shared/Text';
 import styles from './index.module.scss';
 
 const Scene5 = ({ transitionData }) => {
-  const step = UseStepper(transitionData, [3, 6, 10]);
+  const {
+    currentStep,
+    stepTwo,
+    stepThree,
+    stepFour,
+  } = UseTransitionStepper(transitionData, [1, 30, 50, 80, 100]);
 
   return (
     <MotionDiv transitionData={transitionData}>
       <div
         className={cx(styles.section, {
-          [styles.stepTwo]: step === 2,
-          [styles.stepThree]: step === 3,
+          [styles.stepThree]: currentStep === 3,
+          [styles.stepFour]: currentStep === 4,
+          [styles.stepFive]: currentStep === 5,
         })}
       >
         <div className={styles.backdrop1}>
@@ -28,7 +35,40 @@ const Scene5 = ({ transitionData }) => {
             quality={100}
           />
         </div>
-        <div className={styles.backdrop2}>
+        <div
+          className={styles.backdrop4}
+          style={{
+            opacity:
+              currentStep < 3
+                ? '0'
+                : currentStep === 3
+                ? `${stepThree / 100}`
+                : currentStep === 4
+                ? `${(100 - stepFour) / 100}`
+                : '1',
+          }}
+        >
+          <Image
+            src="/assets/images/scene5/7.jpg"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            objectPosition="bottom left"
+            loading="eager"
+            quality={100}
+          />
+        </div>
+        <div
+          className={styles.backdrop2}
+          style={{
+            opacity:
+              currentStep < 4
+                ? '0'
+                : currentStep === 4
+                ? `${stepFour / 100}`
+                : '1',
+          }}
+        >
           <Image
             src="/assets/images/scene5/2-1.jpg"
             alt=""
@@ -39,7 +79,17 @@ const Scene5 = ({ transitionData }) => {
             quality={100}
           />
         </div>
-        <div className={styles.characterRight}>
+        <div
+          className={styles.characterRight}
+          style={{
+            right:
+              currentStep < 4
+                ? '-50%'
+                : currentStep === 4
+                ? `${-50 + 0.64 * stepFour}%`
+                : '14%',
+          }}
+        >
           <img src="/assets/images/scene5/4.png" alt="" />
         </div>
         <div className={styles.backdrop3}>
@@ -53,7 +103,17 @@ const Scene5 = ({ transitionData }) => {
             quality={100}
           />
         </div>
-        <div className={styles.characterLeft}>
+        <div
+          className={styles.characterLeft}
+          style={{
+            left:
+              currentStep === 1
+                ? '16%'
+                : currentStep === 2
+                ? `${16 + 0.08 * stepTwo}%`
+                : '24%',
+          }}
+        >
           <img src="/assets/images/scene5/3.png" alt="" />
           <div className={styles.hand}>
             <img src="/assets/images/scene5/5.png" alt="" />
@@ -64,14 +124,32 @@ const Scene5 = ({ transitionData }) => {
           bottom="20%"
           withBg
           className={styles.text}
-          visible={step === 2}
+          visible={currentStep === 2 || currentStep === 3}
+          style={{
+            right:
+              currentStep === 1
+                ? '19%'
+                : currentStep === 2
+                ? `${19 - 0.09 * stepTwo}%`
+                : '10%',
+          }}
         >
           After a few years of honing his drawing and writing styles, he would
           cut stencils and go out and paint them in the streets over the
           weekend. Posting these stencils up began to give Scrappy a boosted
           selfconfdence, something teenagers of his age can struggle with.
         </Text>
-        <div className={styles.car}>
+        <div
+          className={styles.car}
+          style={{
+            left:
+              currentStep < 3
+                ? '-300vh'
+                : currentStep === 3
+                ? `${-300 + 5.5 * stepThree}vh`
+                : '250vh',
+          }}
+        >
           <img src="/assets/images/scene5/6.png" alt="" />
         </div>
       </div>
