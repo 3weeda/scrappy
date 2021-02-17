8f205cb726215b9c82f3ff5874b1bb6508908dd6
3weeda <3weeda@gmail.com>
2021-02-17 18:09:43 +0200

    SYFT-956: add main soundtrack on loop

--- layout/Header/index.js
+++ layout/Header/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import styles from './index.module.scss';
@@ -11,54 +10,48 @@ const links = [
   { icon: 'discord', url: 'www.discord.com' },
 ];
 
-const Header = ({ hideLogo }) => {
-  const [soundOn, setSoundOn] = useState(true);
-
-  const toggleSound = () => {
-    // TODO: add sound
-    setSoundOn(!soundOn);
-  };
-
-  return (
-    <div className={styles.header}>
-      <div className={styles.headerCont}>
-        <div className={styles.links}>
-          {links.map(({ icon, url }) => (
-            <a key={icon} href={url} target="_blank" rel="noreferrer noopener">
-              <img src={`/assets/images/icons/${icon}-icon.svg`} alt="" />
-            </a>
-          ))}
+const Header = ({ hideLogo, playing, toggle }) => (
+  <div className={styles.header}>
+    <div className={styles.headerCont}>
+      <div className={styles.links}>
+        {links.map(({ icon, url }) => (
+          <a key={icon} href={url} target="_blank" rel="noreferrer noopener">
+            <img src={`/assets/images/icons/${icon}-icon.svg`} alt="" />
+          </a>
+        ))}
+      </div>
+      {!hideLogo ? (
+        <div className={styles.logo}>
+          <img src="/assets/images/intro/logo-red.png" alt="" />
         </div>
-        {!hideLogo ? (
-          <div className={styles.logo}>
-            <img src="/assets/images/intro/logo-red.png" alt="" />
+      ) : null}
+      <div className={styles.options}>
+        <div className={styles.text}>
+          <div className={styles.greet}>
+            <span>Hi there..</span>
+            <img src="/assets/images/say-hi.svg" alt="Hi there!" />
           </div>
-        ) : null}
-        <div className={styles.options}>
-          <div className={styles.text}>
-            <div className={styles.greet}>
-              <span>Hi there..</span>
-              <img src="/assets/images/say-hi.svg" alt="Hi there!" />
-            </div>
-            <span className={styles.phrase}>Say hi.</span>
-          </div>
-          <button
-            type="button"
-            className={cx(styles.sound, { [styles.turnOff]: !soundOn })}
-            onClick={toggleSound}
-          />
+          <span className={styles.phrase}>Say hi.</span>
         </div>
+        <button
+          type="button"
+          className={cx(styles.sound, { [styles.turnOff]: !playing })}
+          onClick={toggle}
+        />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 Header.propTypes = {
   hideLogo: PropTypes.bool,
+  playing: PropTypes.bool,
+  toggle: PropTypes.func.isRequired,
 };
 
 Header.defaultProps = {
   hideLogo: false,
+  playing: true,
 };
 
 export default Header;
